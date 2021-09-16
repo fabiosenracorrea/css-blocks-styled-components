a0968a546ffe70619888a9bd236cfd2564af3a1d
Fábio S. Corrêa <fabiosenracorrea@hotmail.com>
2021-09-16 17:12:33 -0300

    up testings

---
+++ lib/esm/index.d.ts
@@ -0,0 +1,4 @@
+export declare const flexAlignCenter: import("styled-components").FlattenSimpleInterpolation;
+export declare const flexBetween: import("styled-components").FlattenSimpleInterpolation;
+export declare const flexMiddle: import("styled-components").FlattenSimpleInterpolation;
+export declare const flexJustifyCenter: import("styled-components").FlattenSimpleInterpolation;
