5a95bc71e45ab770c9f31600e35c56e6bbb05d69
Fábio S. Corrêa <fabiosenracorrea@hotmail.com>
2021-09-17 13:49:20 -0300

    lib removed from repo

--- lib/esm/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './displays';
-export * from './appearance';
-export * from './marginPaddings';
-export * from './displays';
-export * from './sizing';
--- lib/esm/utils/constants.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const PIXEL_TO_REM = 16;
-export declare const EMPTY_STRING = "";
