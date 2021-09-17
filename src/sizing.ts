eef612a198a2c33364dcc503fa5a4025677fa2d9
Fábio S. Corrêa <fabiosenracorrea@hotmail.com>
2021-09-17 08:13:16 -0300

    sizing added

---
+++ src/sizing.ts
@@ -0,0 +1,41 @@
+import { css, FlattenSimpleInterpolation } from 'styled-components';
+
+import { PIXEL_TO_REM } from './utils/constants';
+
+export type PixelStringOptions =
+  | 'auto'
+  | 'fit-content'
+  | 'min-content'
+  | 'max-content'
+  | 'inherit'
+  | 'unset';
+
+export type PixelOptions = number | PixelStringOptions;
+
+export interface BoxGetterOptions {
+  useRem?: boolean;
+}
+
+export function getPixel(px: PixelOptions): string {
+  if (typeof px === 'string') return px;
+
+  return `${px / PIXEL_TO_REM}px`;
+}
+
+export function convertPixelToRem(px: PixelOptions): string {
+  if (typeof px === 'string') return px;
+
+  return `${px / PIXEL_TO_REM}rem`;
+}
+
+export const getBoxWidthAndHeight = (
+  boxSize: number,
+  { useRem }: BoxGetterOptions = {},
+): FlattenSimpleInterpolation => {
+  const size = useRem ? convertPixelToRem(boxSize) : `${boxSize}px`;
+
+  return css`
+    height: ${size};
+    width: ${size};
+  `;
+};
